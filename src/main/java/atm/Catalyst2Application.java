05f56d313469fca783f30a1e1cf0b67947b37e23
Ryan Day <jrday@users.noreply.github.com>
2018-08-21 12:57:29 -0700

    Initial commit from Atomist

---
+++ src/main/java/atm/Catalyst2Application.java
@@ -0,0 +1,12 @@
+package atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Catalyst2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Catalyst2Application.class, args);
+	}
+}
